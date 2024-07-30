06483a38c2288c67412d7a2ee5577eeed0d53a84
Jose Herminio Parreno Piqueras <parrenjh@basfad.basf.net>
2024-07-30 16:57:41 +0200

    feat: some progress

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const _customColor = Color.fromARGB(255, 201, 42, 153);
+
+const List<Color> _colors = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.teal,
+  Colors.purple,
+  Colors.orange,
+  _customColor
+];  
+
+class AppTheme {
+
+  final int selectedColor; 
+
+  AppTheme({this.selectedColor = 0}): assert(selectedColor >= 0 && selectedColor < _colors.length, 'Invalid color index');
+
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.red
+    );
+  }
+}
