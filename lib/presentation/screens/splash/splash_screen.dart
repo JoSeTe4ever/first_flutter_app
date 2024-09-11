3a8194d18c7982703751e21b35fdf6ccde30365d
Jose Herminio Parreno Piqueras <jose-herminio.parreno@basf.com>
2024-09-11 10:24:12 +0200

    feat: add the splash screen

---
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SwipeablePageView extends StatefulWidget {
+  const SwipeablePageView({super.key});
+
+  @override
+  State<SwipeablePageView> createState() => _SwipeablePageViewState();
+}
+
+class _SwipeablePageViewState extends State<SwipeablePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
